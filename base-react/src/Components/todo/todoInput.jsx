61a220cc891d3aa446815692c3f7f83a9e9f3253
caoanhvu <cavu5541@gmail.com>
2025-06-22 00:44:42 +0700

    commit change code

--- base-react/src/Components/todo/todoInput.jsx
+++ base-react/src/Components/todo/todoInput.jsx
@@ -4,10 +4,19 @@ const TodoInput = (props) => {
     console.log("check props: ",props);
     const {addNewToDo} = props;
     // addNewToDo("Vũ Ruồi");
+
+    const handleFunction = () => {
+        alert("Bấm vào cho anh");
+    }
+
+    const handleOnChange = (name) => {
+        console.log("Handle on change",name);
+    }
+
     return (
         <div className='todo-input'>
-        <input type="text" className='input-enter' placeholder='enter your task'/>
-        <button className='btn-add'>Add</button>
+        <input type="text" className='input-enter' placeholder='enter your task' onChange={(event) => {handleOnChange(event.target.value)}}/>
+        <button className='btn-add' onClick={handleFunction}>Add</button>
       </div>
     )
 }
