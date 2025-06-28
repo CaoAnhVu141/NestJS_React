4264cec2902711212e455e2ae716d5fd96e9fac1
caoanhvu <cavu5541@gmail.com>
2025-06-29 01:13:01 +0700

    update commit code

--- base-react/src/App.jsx
+++ base-react/src/App.jsx
@@ -7,33 +7,37 @@ import imglogo from './assets/react.svg'
 
 function App() {
 
-  const [dataTodoList,setTodoList] = useState([
-      // {id: 1, name: "Cao Anh Vũ"},
-      // {id:2,name: "Nguyễn Văn Bấc"}
+  const [dataTodoList, setTodoList] = useState([
+    // {id: 1, name: "Cao Anh Vũ"},
+    // {id:2,name: "Nguyễn Văn Bấc"}
   ]);
 
   const addNewToDo = (name) => {
     const newDataTodo = {
-        id: randomIntFromInterval(1,100),
-        name: name,
+      id: randomIntFromInterval(1, 100),
+      name: name,
     }
-    setTodoList([...dataTodoList,newDataTodo]);
+    setTodoList([...dataTodoList, newDataTodo]);
   }
 
-  const randomIntFromInterval = (min, max) => { 
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+  const randomIntFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
-  return(
+  return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
-      <TodoInput 
-       addNewToDo={addNewToDo}/>
-      <TodoData
-      dataTodoList={dataTodoList}/>
-      <div className='todo-image'>
-        <img src={imglogo} alt="" />
-      </div>
+      <TodoInput
+        addNewToDo={addNewToDo} />
+
+      {dataTodoList.length > 0 ?
+        <TodoData
+          dataTodoList={dataTodoList} />
+        :
+        <div className='todo-image'>
+          <img src={imglogo} alt="" />
+        </div>
+      }
     </div>
   )
 }
--- base-react/src/Components/todo/todoInput.jsx
+++ base-react/src/Components/todo/todoInput.jsx
@@ -19,9 +19,6 @@ const TodoInput = (props) => {
         <div className='todo-input'>
         <input type="text" className='input-enter' placeholder='enter your task' onChange={(event) => {handleOnChange(event.target.value)}} value={valueInput}/>
         <button className='btn-add' onClick={handleFunction}>Add</button>
-        <div>
-            My name is: {valueInput}
-        </div>
       </div>
     )
 }
