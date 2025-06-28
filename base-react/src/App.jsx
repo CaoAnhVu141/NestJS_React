c5cb72623cd72581b1e14eff077bc66a9dac7b0f
caoanhvu <cavu5541@gmail.com>
2025-06-29 01:01:18 +0700

    update commit code

--- base-react/src/App.jsx
+++ base-react/src/App.jsx
@@ -6,16 +6,10 @@ import TodoInput from './Components/todo/todoInput'
 import imglogo from './assets/react.svg'
 
 function App() {
-  const name = "Ruoi";
-  const age = 10;
-  const data = {
-    address: "hanoi",
-    country: "Vietnam"
-  }
 
   const [dataTodoList,setTodoList] = useState([
-      {id: 1, name: "Cao Anh Vũ"},
-      {id:2,name: "Nguyễn Văn Bấc"}
+      // {id: 1, name: "Cao Anh Vũ"},
+      // {id:2,name: "Nguyễn Văn Bấc"}
   ]);
 
   const addNewToDo = (name) => {
@@ -30,16 +24,12 @@ function App() {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-
   return(
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoInput 
        addNewToDo={addNewToDo}/>
       <TodoData
-      name={name}
-      age={age}
-      data={data}
       dataTodoList={dataTodoList}/>
       <div className='todo-image'>
         <img src={imglogo} alt="" />
--- base-react/src/Components/todo/todoData.jsx
+++ base-react/src/Components/todo/todoData.jsx
@@ -7,19 +7,13 @@ const TodoData = (props) => {
         {props.dataTodoList.map((item,index) => {
           return (
             <>
-            <div className="show-data-todo">
+            <div className="show-data-todo" key={item.id}>
             <div>{item.name}</div>
             <button className="btn-delete-item">Delete</button>
             </div>
             </>
           )
         })}
-        <div>My name is {name}</div>
-        <div>Age: {age}</div>
-        <div>Data 02</div>
-        <div>
-          {JSON.stringify(props.dataTodoList)}
-        </div>
       </div>
     )
 }
