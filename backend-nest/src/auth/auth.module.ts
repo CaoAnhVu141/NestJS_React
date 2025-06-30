a38b14af3f9cee793d6b4a23c3a03071e3323206
caoanhvu <cavu5541@gmail.com>
2025-06-30 23:41:08 +0700

    update commit code

--- backend-nest/src/auth/auth.module.ts
+++ backend-nest/src/auth/auth.module.ts
@@ -4,9 +4,27 @@ import { UsersService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './passport/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService,LocalStrategy],
-  imports: [UsersModule, PassportModule]
+  providers: [AuthService,LocalStrategy,JwtStrategy],
+  exports: [AuthService],
+  imports: [UsersModule, PassportModule,
+
+  // config register for jwt 
+  JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRE'),
+        },
+      }),
+
+  inject: [ConfigService],
+}),
+  ]
 })
 export class AuthModule {}
