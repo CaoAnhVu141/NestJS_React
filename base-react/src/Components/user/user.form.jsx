3a6c05d3e278f37b4d913c126b9831ef727ca15e
caoanhvu <cavu5541@gmail.com>
2025-07-02 20:44:55 +0700

    update commit code

---
+++ base-react/src/Components/user/user.form.jsx
@@ -0,0 +1,50 @@
+import { Button, Flex, Input } from "antd";
+import { useState } from "react";
+
+const UserForm = () => {
+    const [fullName,setFullName] = useState();
+    const [email,setEmail] = useState();
+    const [password,setPassword] = useState();
+    const [age,setAge] = useState();
+    const [gender,setGender] = useState();
+    console.log("check full name: ",email);
+
+    const handleClickBtn = () => {
+        alert("Bú");
+    }
+
+    return (
+        <div className="user-form" style={{ display: "flex", gap: "10px",marginTop: "10px", justifyContent: "center" }}>
+                <div>
+                    <span>Họ tên</span>
+                    <Input placeholder="Họ tên"
+                    value={fullName} onChange={(event) => {setFullName(event.target.value)}}/>
+                </div>
+                <div>
+                    <span>Email</span>
+                    <Input placeholder="Email"
+                    value={email} onChange={(event) => {setEmail(event.target.value)}}/>
+                </div>
+                <div>
+                    <span>Mật khẩu</span>
+                    <Input.Password placeholder="Mật khẩu"
+                    value={password} onChange={(event) => {setPassword(event.target.value)}}/>
+                </div>
+                <div>
+                    <span>Tuổi</span>
+                    <Input placeholder="Tuổi"
+                    value={age} onChange={(event) => {setAge(event.target.value)}}/>
+                </div>
+                <div>
+                    <span>Giới tính</span>
+                    <Input placeholder="Giới tính"
+                    value={gender} onChange={(event) => {setGender(event.target.value)}}/>
+                </div>
+                <div>
+                    <Button type="dashed" onClick={() => {handleClickBtn()}}>Dashed Button</Button>
+                </div>
+        </div>
+    )
+}
+
+export default UserForm;
--- base-react/src/pages/user.jsx
+++ base-react/src/pages/user.jsx
@@ -1,7 +1,12 @@
+import UserForm from "../Components/user/user.form";
+import UserTable from "../Components/user/user.table";
 
 const UserPage = () => {
     return (
-        <div>User Page</div>
+        <div>
+            <UserForm/>
+            <UserTable/>
+        </div>
     )
 }
 
