241a8acb3d2f7654ee2f343bad6d5515dfc0610b
caoanhvu <cavu5541@gmail.com>
2025-07-17 22:00:33 +0700

    commit code

--- base-react/src/App.jsx
+++ base-react/src/App.jsx
@@ -3,8 +3,26 @@ import Header from './Components/layout/header';
 import Footer from './Components/layout/footer';
 import './Components/todo/todo.css'
 import { Outlet } from 'react-router-dom';
+import { getAccountAPI } from './services/api.service';
+import { useContext, useEffect } from 'react';
+import { AuthContext } from './Components/context/auth.context';
 
 function App() {
+
+  useEffect(() => {
+      getHandleAccount();
+  },[])
+
+  // set userLogin when refresh
+  const {setUserLogin} = useContext(AuthContext);
+
+  // when refresh (f5) no delete data
+  const getHandleAccount = async () => {
+      const response = await getAccountAPI();
+      if(response.data){
+        setUserLogin(response.data.user)
+      }
+  }
   return (
     <>
       <Header />
--- base-react/src/services/api.service.js
+++ base-react/src/services/api.service.js
@@ -67,6 +67,13 @@ const LoginUserAPI =  (email,password) => {
         }   
        return axios.post(URL_BACKEND,data);
 }
+
+const getAccountAPI =  () => {
+    const URL_BACKEND = "/api/v1/auth/account"; 
+       return axios.get(URL_BACKEND);
+}
     
 
-export {CreateUserAPI,GetAllUserAPI, UpdateUserAPI, DeleteUserAPI,HandleUploadFileAPI,UpdateAvatarUserAPI,RegisterUserAPI,LoginUserAPI};
+export {CreateUserAPI,GetAllUserAPI, UpdateUserAPI, DeleteUserAPI,HandleUploadFileAPI,UpdateAvatarUserAPI,RegisterUserAPI,LoginUserAPI,
+    getAccountAPI
+};
