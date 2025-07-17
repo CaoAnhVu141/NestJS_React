5810d12a421ac307a9e3ca70bd69f868f2862a11
caoanhvu <cavu5541@gmail.com>
2025-07-17 21:17:14 +0700

    update code

--- base-react/src/Components/layout/header.jsx
+++ base-react/src/Components/layout/header.jsx
@@ -1,40 +1,56 @@
 
 import { Link, NavLink } from 'react-router-dom';
-import { AppstoreOutlined, BookOutlined, HomeOutlined, MailOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
+import { AlibabaOutlined, AppstoreOutlined, BookOutlined, HomeOutlined, LoginOutlined, MailOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
-import { useContext, useState } from 'react';
+import { Children, useContext, useState } from 'react';
 import { AuthContext } from '../context/auth.context';
 
 const Header = () => {
-        const [current, setCurrent] = useState('');
-        
-        const onClick = e => {
-            console.log('click ', e);
-            setCurrent(e.key);
-        };
-        const {userLogin} = useContext(AuthContext);
-        console.log("check user: ", userLogin);
+    const [current, setCurrent] = useState('');
 
-        const items = [
-            {
-                label: <Link to={"/"}>Home</Link>,
-                key: 'home',
-                icon: <HomeOutlined/>,
-            },
-            {
-                label: <Link to={"/user"}>User</Link>,
-                key: 'user',
-                icon: <UserOutlined/>,
-            },
-            {
-                label: <Link to={"/book"}>Book</Link>,
-                key: 'book',
-                icon: <BookOutlined/>,
-            },
-        ];
-        return (
-            <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
-        )
-    }
+    const onClick = e => {
+        setCurrent(e.key);
+    };
+    const { userLogin } = useContext(AuthContext);
+    console.log("check user: ", userLogin);
+
+    const items = [
+        {
+            label: <Link to={"/"}>Home</Link>,
+            key: 'home',
+            icon: <HomeOutlined />,
+        },
+        {
+            label: <Link to={"/user"}>User</Link>,
+            key: 'user',
+            icon: <UserOutlined />,
+        },
+        {
+            label: <Link to={"/book"}>Book</Link>,
+            key: 'book',
+            icon: <BookOutlined />,
+        },
+        ...(!userLogin._id ? [{
+            label: <Link to={"/login"}>Đăng nhập</Link>,
+            key: 'login',
+            icon: <LoginOutlined />,
+        }] : []),
+
+        ...(userLogin._id ? [{
+            label: `Welcome ${userLogin.name}`,
+            key: 'logout',
+            icon: <AlibabaOutlined />,
+            children: [
+                {
+                    label: "Đăng xuất",
+                    key: "logout"
+                }
+            ]
+        }] : []),
+    ];
+    return (
+        <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+    )
+}
 export default Header;
 
